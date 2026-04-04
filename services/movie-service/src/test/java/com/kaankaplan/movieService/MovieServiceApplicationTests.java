3be13fd9628371e5a6a5ed1c7c75b231d63f6797
Wadgua <wadguakillian@gmail.com>
2026-04-04 17:11:50 +0100

    feat: movie-service added completely1

--- services/movie-service/src/test/java/com/kaankaplan/movieService/MovieServiceApplicationTests.java
+++ services/movie-service/src/test/java/com/kaankaplan/movieService/MovieServiceApplicationTests.java
@@ -2,7 +2,7 @@ package com.kaankaplan.movieService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+// hello
 @SpringBootTest
 class MovieServiceApplicationTests {
 
