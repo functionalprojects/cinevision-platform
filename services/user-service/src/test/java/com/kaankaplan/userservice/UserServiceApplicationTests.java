02a609f6ec6ccfed9bce3c5009c640cb4aada6db
Wadgua <wadguakillian@gmail.com>
2026-04-05 10:26:39 +0100

    feat: changes to userservice

--- services/user-service/src/test/java/com/kaankaplan/userservice/UserServiceApplicationTests.java
+++ services/user-service/src/test/java/com/kaankaplan/userservice/UserServiceApplicationTests.java
@@ -2,7 +2,7 @@ package com.kaankaplan.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+// hello
 @SpringBootTest
 class UserServiceApplicationTests {
 
