9a6103a00b38f250f9010d5a711db586eba370d1
Wadgua <wadguakillian@gmail.com>
2026-04-05 08:52:54 +0100

    feat: changes to email-service287

--- services/email-service/src/test/java/com/kaankaplan/emailService/EmailServiceApplicationTests.java
+++ services/email-service/src/test/java/com/kaankaplan/emailService/EmailServiceApplicationTests.java
@@ -8,6 +8,6 @@ class EmailServiceApplicationTests {
 
     @Test
     void contextLoads() {
-        // Basic test to  verifyb  Spring context loads
+        // Basic test to  verify Spring context loads
     }
 }
