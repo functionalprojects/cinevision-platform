dcecb71d2f6a14e8062062242c2c48ef4474ab36
Wadgua <wadguakillian@gmail.com>
2026-04-06 22:46:39 +0100

    feat: changes to -gateway1253624

--- services/api-gateway/src/test/java/com/kaankaplan/apigateway/ApiGatewayApplicationTests.java
+++ services/api-gateway/src/test/java/com/kaankaplan/apigateway/ApiGatewayApplicationTests.java
@@ -9,6 +9,6 @@ class ApiGatewayApplicationTests {
     @Test
     void contextLoads() {
         // Basic test to ensure  Spring context  loads successfully today
-         // Basic test to ensure  Spring context loads  successfully today
+         // Basic test to ensure  Spring context  loads  successfully today
     }
 }
